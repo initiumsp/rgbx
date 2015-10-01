cda287d321b330ddb76322972f047651304b9c61
Andy Shu Xin <andyshuxin@gmail.com>
2015-10-01 17:33:26 +0800

    Use parameters with reference

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,16 +12,22 @@
 
   const deuteranopia = data => {
 
+    // Reference parameters from:
+    // http://blog.noblemaster.com/wp-content/uploads/2013/10/2013-10-26-ColorCorrection.txt
+
     for (var i = 0; i < data.length; i += 4) {
 
-      let yellow = (data[i] + data[i + 1]) / 2;
+      let getR = (r, g, b) => 0.43 * r + 0.72 * g + -0.15 * b;
+      let getG = (r, g, b) => 0.34 * r + 0.57 * g + -0.09 * b;
+      let getB = (r, g, b) => -0.02 * r + 0.03 * g + 1.00 * b;
 
-      // red
-      data[i] = yellow;
-      // green
-      data[i + 1] = yellow;
-      // blue - remain constant
-      // data[i + 2] = data[i + 2];
+      let r = data[i];
+      let g = data[i + 1];
+      let b = data[i + 2];
+
+      data[i] = getR(r, g, b);
+      data[i+1] = getG(r, g, b);
+      data[i+2] = getB(r, g, b);
     }
   };
 
