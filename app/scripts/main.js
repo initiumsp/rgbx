c280ce0b8465af46ec008eca528dababc8b27e5e
Andy Shu Xin <andyshuxin@gmail.com>
2015-10-04 15:09:11 +0800

    add LMS-related parameters

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -31,6 +31,18 @@
     [-0.02, 0.03,  0.80]
   ];
 
+  const rgbTolmsParameters = [
+    [17.8824, 43.5161, 4.11935],
+    [3.45565, 27.1554, 3.86714],
+    [0.0299566, 0.184309, 1.46709]
+  ];
+
+  const lmsTorgbParameters = [
+    [0.0809444479, -0.130504409, 0.116721066],
+    [-0.0102485335, 0.0540193266, -0.113614708],
+    [-0.000365296938, -0.00412161469, 0.693511405]
+  ];
+
   const add = (sum, operand) => sum + operand;
 
   const matrixVectorProduct = (mat, vec) =>
@@ -76,7 +88,6 @@
 
     };
   }
-
 
   let filter = createLinearFilter(deuteranopiaLinearParameters);
 
